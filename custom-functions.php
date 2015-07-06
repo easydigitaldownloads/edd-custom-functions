91798cf9fe59652a46ac0b080fe797e3d97b36aa
Pippin Williamson <pippin@pippinsplugins.com>
2015-07-06 16:28:22 -0500

    Add snap engage code

--- custom-functions.php
+++ custom-functions.php
@@ -162,6 +162,30 @@ function edd_ga_tracking_code() {
 add_action( 'wp_footer', 'edd_ga_tracking_code' );
 
 
+function edd_snap_engage_code() {
+?>
+<!-- begin SnapEngage code -->
+<script type="text/javascript">
+  (function() {
+    var se = document.createElement('script'); se.type = 'text/javascript'; se.async = true;
+    se.src = '//storage.googleapis.com/code.snapengage.com/js/d6358744-b7cc-4b25-a5e0-ed98d5e10990.js';
+    var done = false;
+    se.onload = se.onreadystatechange = function() {
+      if (!done&&(!this.readyState||this.readyState==='loaded'||this.readyState==='complete')) {
+        done = true;
+        /* Place your SnapEngage JS API code below */
+        /* SnapEngage.allowChatSound(true); Example JS API: Enable sounds for Visitors. */
+      }
+    };
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
+  })();
+</script>
+<!-- end SnapEngage code -->
+<?php
+}
+add_action( 'wp_footer', 'edd_snap_engage_code' );
+
+
 /**
  * Pre-populates radios based on previous name field
  * Searches post type 'person'
